4f785784f1b16143ea0aa967b47f8fe140f0b965
Daniel "Ypek" Merib <81493681+ypek@users.noreply.github.com>
2022-06-25 18:52:30 -0300

    calculadora com todos seus metodos testados

--- Tdd's/specs/calculadora_spec.rb
+++ Tdd's/specs/calculadora_spec.rb
@@ -50,9 +50,13 @@ describe('calculadora') do
     it("Deve dividir dois inteiros positivos")do #testa a divisao de dois inteiros positivos
         expect(calculadora.divisao(4,5)).to eq 0
     end
+
+    
     it("Deve dividir um numero qualquer com 0")do #testa a divisao de um inteiro com 0
-        expect(calculadora.divisao(9,0)).to eq "Não é possível dividir por 0!" ######Duvida do que colocar aqui!!!#####
+        expect(calculadora.divisao(9,0)).to eq "Não é possível dividir por 0!" #ZeroDivisionError
     end
+
+    
     it("Deve dividir numeros negativos")do #testa a divisao de dois numeros negativos
         expect(calculadora.divisao(-10,-2)).to eq 5
     end
@@ -64,9 +68,13 @@ describe('calculadora') do
     it("Deve calcular a raiz quadrada de um numero positivo")do #testa a raiz quadrada de um numero positivo
         expect(calculadora.raiz(9)).to eq 3
     end
+
+
     it("Deve calcular a raiz quadrada de um numero negativo")do #testa a raiz quadrada de um numero negativo
-        expect(calculadora.raiz(-9)).to eq "Não é possível calcular a raiz quadrada de um número negativo!" ######Duvida do que colocar aqui!!!#####
-    end
+        expect(calculadora.raiz(-9)).to eq "Não é possível calcular a raiz de um número negativo!" ######Duvida do que colocar aqui!!!#####
+    end  #Math::DomainError
+
+
     it("Deve calcular a raiz quadrada de um numero qualquer")do #testa a raiz quadrada de um numero qualquer
         expect(calculadora.raiz(4)).to eq 2
     end
@@ -81,6 +89,36 @@ describe('calculadora') do
     it("Deve calcular a potencia de um numero qualquer")do #testa a potencia de um numero qualquer
         expect(calculadora.potencia(4,2)).to eq 16
     end
+        ###################################################################################
+            #usando metodo seno coscendo e tangente
+    it("Deve calcular o seno de um numero positivo")do #testa o seno de um numero positivo
+        expect(calculadora.seno(90)).to eq 0.8939966636005579
+    end
+    it("Deve calcular o seno de um numero negativo")do #testa o seno de um numero negativo
+        expect(calculadora.seno(-90)).to eq -0.8939966636005579
+    end
+    it("Deve calcular o seno de um numero qualquer")do #testa o seno de um numero qualquer
+        expect(calculadora.seno(45)).to eq 0.8509035245341184
+    end
+    it("Deve calcular o cosseno de um numero positivo")do #testa o cosseno de um numero positivo
+        expect(calculadora.cosseno(90)).to eq  -0.4480736161291701
+    end
+    it("Deve calcular o cosseno de um numero negativo")do #testa o cosseno de um numero negativo
+        expect(calculadora.cosseno(-90)).to eq -0.4480736161291701
+    end
+    it("Deve calcular o cosseno de um numero qualquer")do #testa o cosseno de um numero qualquer
+        expect(calculadora.cosseno(45)).to eq 0.5253219888177297
+    end
+    it("Deve calcular a tangente de um numero positivo")do #testa a tangente de um numero positivo
+        expect(calculadora.tangente(90)).to eq -1.995200412208242
+    end
+    it("Deve calcular a tangente de um numero negativo")do #testa a tangente de um numero negativo
+        expect(calculadora.tangente(-90)).to eq 1.995200412208242
+    end
+    it("Deve calcular a tangente de um numero qualquer")do #testa a tangente de um numero qualquer
+        expect(calculadora.tangente(45)).to eq 1.6197751905438615
+    end
+    ###################################################################################
 end
 
 
--- Tdd's/src/calculadora.rb
+++ Tdd's/src/calculadora.rb
@@ -13,14 +13,30 @@ class Calculadora
 
     def divisao(a,b) #metodo de divisao
     a / b
+    rescue ZeroDivisionError
+        "Não é possível dividir por 0!"
     end
 
     def raiz(a) #metodo de raiz
     Math.sqrt(a)
+    rescue Math::DomainError
+        "Não é possível calcular a raiz de um número negativo!"
     end
 
     def potencia(a,b) #metodo de potencia
     a ** b
     end
+
+    def seno(a) #metodo de seno
+    Math.sin(a)
+    end
+
+    def cosseno(a) #metodo de cosseno
+    Math.cos(a)
+    end
+
+    def tangente(a) #metodo de tangente
+    Math.tan(a)
+    end 
 
 end
