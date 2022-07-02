6a59c0eb94be2e899b5b67c22b9085e537efdeaa
Daniel "Ypek" Merib <81493681+ypek@users.noreply.github.com>
2022-07-01 21:24:59 -0300

    att calc

--- Tdd's/specs/calculadora_spec.rb
+++ Tdd's/specs/calculadora_spec.rb
@@ -49,14 +49,23 @@ describe('calculadora') do
             #usando metodo de divisao
     it("Deve dividir dois inteiros positivos")do #testa a divisao de dois inteiros positivos
         expect(calculadora.divisao(4,5)).to eq 0
-    end
-
-    
+<<<<<<< HEAD
+    end    
     it("Deve dividir um numero qualquer com 0")do #testa a divisao de um inteiro com 0
         expect(calculadora.divisao(9,0)).to eq "Não é possível dividir por 0!" #ZeroDivisionError
     end
-
-    
+=======
+    end
+    it("Deve dividir um numero qualquer com 0")do #testa a divisao de um inteiro com 0
+        expect(calculadora.divisao(9,0)).to eq "Não é possível dividir por 0!" #ZeroDivisionError
+    end
+    it("Deve dividir numeros negativos")do #testa a divisao de dois numeros negativos
+        expect(calculadora.divisao(-4,-5)).to eq 0
+    end
+    it('Deve dividir um numero negativo e outro positivo')do #testa a divisao de um numero negativo e outro positivo
+        expect(calculadora.divisao(-4,5)).to eq -1
+    end
+>>>>>>> a1207daf6a638043f41626a937967ab2fae16be5
     it("Deve dividir numeros negativos")do #testa a divisao de dois numeros negativos
         expect(calculadora.divisao(-10,-2)).to eq 5
     end
@@ -68,15 +77,14 @@ describe('calculadora') do
     it("Deve calcular a raiz quadrada de um numero positivo")do #testa a raiz quadrada de um numero positivo
         expect(calculadora.raiz(9)).to eq 3
     end
-
-
     it("Deve calcular a raiz quadrada de um numero negativo")do #testa a raiz quadrada de um numero negativo
         expect(calculadora.raiz(-9)).to eq "Não é possível calcular a raiz de um número negativo!" ######Duvida do que colocar aqui!!!#####
     end  #Math::DomainError
-
-
     it("Deve calcular a raiz quadrada de um numero qualquer")do #testa a raiz quadrada de um numero qualquer
         expect(calculadora.raiz(4)).to eq 2
+    end
+    it("Deve calcular a raiz quadrada de um numero 0")do
+        expect(calculadora.raiz(0)).to eq 0
     end
 
             #usando metodo de potencia
@@ -86,10 +94,14 @@ describe('calculadora') do
     it("Deve calcular a potencia de um numero negativo")do #testa a potencia de um numero negativo
         expect(calculadora.potencia(-9,2)).to eq 81
     end
-    it("Deve calcular a potencia de um numero qualquer")do #testa a potencia de um numero qualquer
-        expect(calculadora.potencia(4,2)).to eq 16
+    it("Deve calcular a potencia de 0")do
+     expect(calculadora.potencia(0,2)).to eq 0
     end
+
+<<<<<<< HEAD
+=======
         ###################################################################################
+>>>>>>> a1207daf6a638043f41626a937967ab2fae16be5
             #usando metodo seno coscendo e tangente
     it("Deve calcular o seno de um numero positivo")do #testa o seno de um numero positivo
         expect(calculadora.seno(90)).to eq 0.8939966636005579
@@ -118,18 +130,155 @@ describe('calculadora') do
     it("Deve calcular a tangente de um numero qualquer")do #testa a tangente de um numero qualquer
         expect(calculadora.tangente(45)).to eq 1.6197751905438615
     end
-    ###################################################################################
-    it('deve fatorar um numero positivo')do #testa o fatorar de um numero positivo
-        expect(calculadora.fatorar(9)).to eq 362880
+    
+<<<<<<< HEAD
+    #usando metodo de fatoracao
+    it("Deve calcular a fatoracao de um numero positivo")do #testa a fatoracao de um numero positivo
+        expect(calculadora.fatorial(9)).to eq 362880
     end
-    it('deve calcular fibonacci de um numero positivo')do #testa o fibonacci de um numero positivo
-        expect(calculadora.fibonacci(9)).to eq 34
+    it("Deve calcular a fatoracao de um numero negativo")do #testa a fatoracao de um numero negativo
+        expect(calculadora.fatorial(-9)).to eq 1  ######Duvida do que colocar aqui!!!#####
     end
-    it('deve calcular numero primo')do #testa o fibonacci de um numero negativo
-        expect(calculadora.primo(9)).to eq false
+    it("Deve calcular a fatoracao de 0")do
+        expect(calculadora.fatorial(0)).to eq 1
     end
 
+    #usando metodo fatorial recursivo
+    it("Deve calcular a fatoracao de um numero positivo")do #testa a fatoracao de um numero positivo
+        expect(calculadora.fatorial_recursivo(9)).to eq 362880
+    end
+    it("Deve calcular a fatoracao de um numero negativo")do #testa a fatoracao de um numero negativo
+        expect(calculadora.fatorial_recursivo(-9)).to eq 1  ######Duvida do que colocar aqui!!!#####
+    end
+    it('Deve calcular a fatoracao de um numero 0')do
+        expect(calculadora.fatorial_recursivo(0)).to eq 1
+    end
 
+    it("Deve retornar pi")do
+        expect(calculadora.pi).to eq 3.141592653589793
+    end
+    it("Deve retornar um numero que nao seja pi")do
+        expect(calculadora.pi).not_to eq 777 #Raffa moreira mano
+    end
+
+    #usando metodo de porcentagem
+    it("Deve calcular a porcentagem de um numero positivo")do #testa a porcentagem de um numero positivo
+        expect(calculadora.porcentagem(70,20)).to eq 14
+    end
+    it("Deve calcular a porcentagem de um numero negativo")do #testa a porcentagem de um numero negativo
+        expect(calculadora.porcentagem(-70,20)).to eq -14
+    end
+    it("Deve calcular a porcentagem de 0")do #testa a porcentagem de um numero qualquer
+        expect(calculadora.porcentagem(0,20)).to eq 0
+    end
+
+    #usando metodo de cubo
+    it("Deve calcular o cubo de um numero positivo")do #testa o cubo de um numero positivo
+        expect(calculadora.cubo(9)).to eq 729
+    end
+    it("Deve calcular o cubo de um numero negativo")do #testa o cubo de um numero negativo
+        expect(calculadora.cubo(-9)).to eq -729
+    end
+    it("Deve calcular o cubo de 0")do
+        expect(calculadora.cubo(0)).to eq 0
+    end
+
+    #usando metodo de delta
+    it("Deve calcular o delta de dois numeros positivos")do #testa o delta de dois numeros positivos
+        expect(calculadora.delta(2,1,1)).to eq -7
+    end
+    it("Deve calcular o delta de dois numeros negativos")do #testa o delta de dois numeros negativos
+        expect(calculadora.delta(-2,-1,-1)).to eq -7
+    end
+
+    #usando metodo logatimo
+    it("Deve calcular o logaritmo de um numero positivo")do #testa o logaritmo de um numero positivo
+        expect(calculadora.logaritmo(2)).to eq 0.301029995663981
+    end
+    it("Deve calcular o logaritmo de um numero negativo")do #testa o logaritmo de um numero negativo
+        expect(calculadora.logaritmo(-2)).to eq "Não é possível calcular o logaritmo de um número negativo!"
+    end
+    it("Deve calcular o logaritmo de 0")do
+        expect(calculadora.logaritmo(0)).to eq -Float::INFINITY
+    end
+    
+    #usando metodo bhaskara
+    it("Deve calcular a Bhaskara de dois numeros positivos")do #testa a Bhaskara de dois numeros positivos
+        expect(calculadora.bhaskara(10,20,-30)).to eq [1.0, -3.0]
+    end
+    it("Deve calcular a Bhaskara quando as raizes nao forem reais")do 
+        expect(calculadora.bhaskara(10,20,30)).to eq "Equação não possui raízes reais"
+    end
+
+    #usando metodo raiz enésima
+    it("Deve calcular a raiz enésima de um numero positivo")do #testa a raiz enésima de um numero positivo
+        expect(calculadora.raiz_enesima(10,36)).to eq 1.0715193052376064
+    end
+    it("Deve calcular a raiz enésima de um numero negativo")do #testa a raiz enésima de um numero negativo
+        expect(calculadora.raiz_enesima(-10,36)).to eq  1.0667638646324789+0.10083887450413744i
+    end
+    it("Deve calcular a raiz enésima de 0")do
+        expect(calculadora.raiz_enesima(0,36)).to eq 0
+    end
+
+    #usando metodo media
+    it("Deve calcular a media de dois numeros positivos")do #testa a media de dois numeros positivos
+        expect(calculadora.media(10,20)).to eq 15
+    end
+    it("Deve calcular a media de dois numeros negativos")do #testa a media de dois numeros negativos
+        expect(calculadora.media(-10,-20)).to eq -15
+    end
+    it("Deve calcular a media de dois numeros iguais")do #testa a media de dois numeros iguais
+        expect(calculadora.media(10,10)).to eq 10
+    end
+    it("Deve calcular a media de 0")do
+        expect(calculadora.media(0,0)).to eq 0
+    end
+
+    #usando metodo moda
+    it("Deve calcular a moda de dois numeros positivos")do #testa a moda de dois numeros positivos
+        expect(calculadora.moda(10,20)).to eq 20
+    end
+    it("Deve calcular a moda de dois numeros negativos")do #testa a moda de dois numeros negativos
+        expect(calculadora.moda(-10,-20)).to eq -10
+    end
+    it("Deve calcular a moda de dois numeros iguais")do #testa a moda de dois numeros iguais
+        expect(calculadora.moda(10,10)).to eq 10
+    end
+    it("Deve calcular a moda de 0")do
+        expect(calculadora.moda(0,0)).to eq 0
+    end
+
+    #usando metodo mediana
+    it("Deve calcular a mediana de dois numeros positivos")do #testa a mediana de dois numeros positivos
+        expect(calculadora.mediana(90,100)).to eq 90
+    end
+    it("Deve calcular a mediana de dois numeros negativos")do #testa a mediana de dois numeros negativos
+        expect(calculadora.mediana(-90,-100)).to eq -100
+    end
+    it("Deve calcular a mediana de dois numeros iguais")do #testa a mediana de dois numeros iguais
+        expect(calculadora.mediana(90,90)).to eq 90
+    end
+    it("Deve calcular a mediana de 0")do
+        expect(calculadora.mediana(0,0)).to eq 0
+    end
+   
+
+
+=======
+        #Metodo de exponensiação
+    
+    it("Deve calcular a exponenciacao de um numero positivo")do #testa a exponenciacao de um numero positivo
+        expect(calculadora.esponencial(9)).to eq 8103.083927575384
+    end
+    it("Deve calcular a exponenciacao de um numero negativo")do #testa a exponenciacao de um numero negativo
+        expect(calculadora.esponencial(-9)).to eq 0.00012340980408667956
+    end
+    it("Deve calcular a exponenciacao de um numero qualquer")do #testa a exponenciacao de um numero qualquer
+        expect(calculadora.esponencial(4)).to eq 54.598150033144236
+    end
+
+>>>>>>> a1207daf6a638043f41626a937967ab2fae16be5
 end
 
 
--- Tdd's/src/calculadora.rb
+++ Tdd's/src/calculadora.rb
@@ -4,11 +4,11 @@ class Calculadora
     end
 
     def subtracao(a,b) #metodo de subtracao
-    if(a < 0 && b < 0)
-        a + b
-    else
-        a - b
-    end
+        if(a < 0 && b < 0)
+            a + b
+        else
+            a - b
+        end
     end
 
     def multiplicacao(a,b) #metodo de multiplicacao
@@ -19,6 +19,7 @@ class Calculadora
     a / b
     rescue ZeroDivisionError
         "Não é possível dividir por 0!"
+
     end
 
     def raiz(a) #metodo de raiz
@@ -43,40 +44,78 @@ class Calculadora
     Math.tan(a)
     end
     
-    def fatorar(a) #metodo de fatoracao
-        if a == 0
-            return 1
+<<<<<<< HEAD
+    def logaritmo(a) #metodo de logaritmo
+    Math.log(a,10).round(15)
+    rescue Math::DomainError
+        "Não é possível calcular o logaritmo de um número negativo!"
+    end
+    
+    def fatorial (n)
+        if (n > 1)
+           return n * fatorial(n - 1)
+        else
+           return 1
         end
-        fator = 1
-        for i in 1..a
-            fator = fator * i
-        end
-        return fator
+=======
+    def esponencial(a) #metodo de esponencial
+    Math.exp(a)
+>>>>>>> a1207daf6a638043f41626a937967ab2fae16be5
     end
 
-    def fibonacci(a) #metodo de fibonacci
-        if a == 0
-            return 0
+    def fatorial_recursivo(n)
+        if (n > 1)
+           return n * fatorial_recursivo(n - 1)
+        else
+           return 1
         end
-        if a == 1
-            return 1
-        end
-        return fibonacci(a-1) + fibonacci(a-2)
     end
 
-    def primo(a) #metodo de primo
-        if a == 1
-            return false
-        end
-        if a == 2
-            return true
-        end
-        for i in 2..a-1
-            if a % i == 0
-                return false
-            end
-        end
-        return true
+    def pi
+        Math::PI
     end
 
+    def porcentagem(a,b)
+        a * b / 100
+    end
+    
+    def cubo(n)
+        n ** 3
+    end
+
+    def delta(a,b,c)
+        b ** 2 - 4 * a * c
+    end
+
+    def bhaskara(a,b,c)
+        x1 = (-b + Math.sqrt(b**2 - 4 * a * c)) / (2 * a)
+        x2 = (-b - Math.sqrt(b**2 - 4 * a * c)) / (2 * a)
+        return x1, x2
+    rescue Math::DomainError
+        "Equação não possui raízes reais"
+    end
+    
+    def raiz_enesima(a,n)
+        a ** (1.0 / n).round(2)
+    end
+
+    def media(a,b)
+        (a + b) / 2
+    end
+
+    def moda(a,b)
+        if a > b
+            return a
+        else
+            return b
+        end
+    end
+
+    def mediana(a,b)
+        if a > b
+            return b
+        else
+            return a
+        end
+    end
 end
